ba5e8b66c116d5dd02d3261c86825c07fbd9a049
Akshayteli-111 <154588023+AkshayBTeli@users.noreply.github.com>
2024-02-15 16:05:26 +0530

    Changes

--- src/app/technician/manageassets/financialanalysis/financialanalysis.js
+++ src/app/technician/manageassets/financialanalysis/financialanalysis.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React from 'react'
 
-export default function FinancialAnalysis() {
+function Finacialanalysis() {
+    return (
+        <>
+            <div className='grid grid-cols-12 gap-[20px] xl:gap-[1.042vw] ml-[32px] xl:ml-[1.667vw]'>
+                <div className='col-span-3 pl-[16px] xl:pl-[0.833vw]'>
+                    <div className='shadow-md shadow-[#0000000D] rounded-lg border border-[#E4E7EC]'>
+                        <div className='text-[#20232E] text-[16px] xl:text-[0.833vw] py-[12px] px-[16px] xl:py-[0.625vw] xl:px-[0.833vw]' >Total Value of All Assets</div>
+                        <div className='border border-[#E4E7EC] px-[12px] py-[6px] xl:px-[0.625vw] xl:py-[0.313vw] text-[#20232E] text-[12px] xl:text-[0.625vw] font-medium bg-[#E4E7EC] rounded-[6px] inline-block'>
+                            Assets Value
+                        </div>
+                        <div className='text-[#3A3E49] text-[36px] xl:text-[1.875vw] font-medium '>$3M</div>
+                        <div className='text-[#84878D] text-[12px] xl:text-[0.625vw] '>Total Value</div>
+                    </div>
+                </div>
+                
+            </div>
 
-  return (
-    <>
-    <div>Financial Analysis</div>
-    </>
-  );
+
+        </>
+    )
 }
+
+export default Finacialanalysis
