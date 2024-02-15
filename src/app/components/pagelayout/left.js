f75100734dd2cfd30d91e4421b24cbfb7d888c8b
nileshnigade <126142968+nileshnigade@users.noreply.github.com>
2024-02-15 12:36:43 +0530

    Changes

--- src/app/components/pagelayout/left.js
+++ src/app/components/pagelayout/left.js
@@ -6,7 +6,6 @@ import 'simplebar-react/dist/simplebar.min.css';
 import { useTheme } from 'next-themes';
 import { Tooltip } from 'primereact/tooltip';
 
-
 export default function Left(props) {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
@@ -49,12 +48,10 @@ export default function Left(props) {
 
 
                   {currentTheme === 'dark' ?
-                    <div>    <Image src="/assets/images/left-menu-icons/logo_dark.png" width={280} height={23} alt='logo icon' /></div>
+                    <div>    <Image src="/assets/images/left-menu-icons/logo_dark.svg" width={280} height={23} alt='logo icon' /></div>
                     :
                     <div>      <Image src="/assets/images/left-menu-icons/logo.svg" width={280} height={23} className='w-[280px] xl:w-[22.583vw]' alt='logo icon' /></div>
                   }
-
-                  <span className='inline-block bg-[#FCF5F6] rounded px-[8px] xl:px-[0.417vw] py-[6px] xl:py-[0.317vw] text-[0.625vw] mt-[18px] whitespace-nowrap dark:bg-[#1C2026]'>Teacher Management Application</span>
                 </Link>
               </div>
               <div className="lock_icon mt-4" data-aos="fade-right" data-aos-offset="500" data-aos-easing="ease-in-sine">
@@ -70,11 +67,6 @@ export default function Left(props) {
               <div data-simplebar>
                 <div className="visible lg:hidden"></div>
                 <ul className="left-menu h-full">
-                  <li className={`${isLinkActive('/') ? 'active' : ''} ico-home`}>
-                    <Link href={'#'}>
-                      <span>Home</span>
-                    </Link>
-                  </li>
                   <li className={`${isLinkActive('/') ? 'active' : ''} ico-dashboard`}>
                     <Link href='#'>
                       <span>Dashboard</span>
@@ -137,7 +129,6 @@ export default function Left(props) {
               <ul className="left-menu hoverNone">
                 <li className="settings"><Link href='/setting'><span>Settings</span></Link></li>
                 <li className='dark-mode'>
-                  
                   <Link href=""> {currentTheme === "dark" ?
                     <span onClick={() => setTheme("light")}>Dark Theme</span>
                     :
@@ -146,35 +137,22 @@ export default function Left(props) {
                 <li className="support mt-[12px] xl:mt-[0.625vw]"><Link href='' className="supportBg"><span>Support Tickets</span></Link></li>
                 <li className="userProfile relative mt-[24px] xl:mt-[1.250vw] -ml-[3px]">
                   <div className="flex items-center" >
-                    {/* <div className="userPic ml-2">
-                      <Image src={profilePicture} width={48} height={48} alt='profile' />
-                    </div> */}
-                    <div className="userPic ml-2" style={{ display: "flex", cursor: 'pointer', minWidth: 48, minHeight: 48, backgroundColor: '#5e656d', borderRadius: 50, justifyContent: 'center', alignItems: 'center' }}>
-                      <div style={{ fontSize: 22, color: 'white', fontWeight: 600, textTransform: 'uppercase' }}>{UserUnit}</div>
-
+                    <div className="userPic ml-2">
+                      <Image src={'/assets/images/left-menu-icons/profile.svg'} width={48} height={48} alt='profile' />
                     </div>
                     <div>
 
                       <Link href=''>
-                        <span className="text-[#4C525F] dark:text-[#FFFFFF] text-[16px] xl:text-[0.833vw] font-semibold block"> {userName} </span>
-                        <span className="text-[#6B7280] dark:text-[#B3B9C6] text-[14px] xl:text-[0.729vw] block"> {usermail} </span>
+                        <span className="text-[#4C525F] dark:text-[#FFFFFF] text-[16px] xl:text-[0.833vw] font-semibold block">Jese Leos</span>
+                        <span className="text-[#6B7280] dark:text-[#B3B9C6] text-[14px] xl:text-[0.729vw] block">Joined in August 2014</span>
                       </Link>
                     </div>
-                    <Link  href=''>
-                    <Tooltip target=".logout" content="Logout" position="top" className="custom-tooltip-content" />
-                      {/* <div className="logout mx-2 bg-[#fff] border border-[#EAE0E0]  hover:bg-[#F0E9EA] p-3" style={{ fontSize: 22, color:currentTheme === 'dark'?"White": 'black', fontWeight: 300, alignItems: 'center'}}> */}
-                        {/* <RiLogoutCircleLine /> */}
-                        <i className='pi pi-sign-out text-[#AF1E27]'></i>
-                      {/* </div> */}
-                      {/* {currentTheme === 'dark' ?
-                        <Image src="/assets/images/left-menu-icons/more_square_dark.svg" width={50} height={50} className='w-[30px] h-[30px]' alt='more_square_dark' />
-                        :
-                        <Image src="/assets/images/left-menu-icons/more_square_white.svg" width={50} height={50} className='w-[30px] h-[30px]' alt='more_square_white' />
-                      } */}
-                    </Link>
+                    
                   </div>
                   <div className="absolute right-0 top-1/2 -translate-y-1/2 userprofileIcon">
-                    {/* <Link href='#'><img src={UserProfile} alt="" /></Link> */}
+                    <Link href='#'>
+                    <Image src="/assets/images/left-menu-icons/more_square_white.svg" width={50} height={50} className='w-[16px] h-[16px]' alt='more_square_white' />
+                      </Link>
                   </div>
                 </li>
               </ul>
