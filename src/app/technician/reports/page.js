4b032b0ab7a82bab41f2f12c7308abd6baef46bb
Akshayteli-111 <154588023+AkshayBTeli@users.noreply.github.com>
2024-02-15 13:14:21 +0530

    Reports cards

--- src/app/technician/reports/page.js
+++ src/app/technician/reports/page.js
@@ -3,21 +3,126 @@ import Image from 'next/image';
 function Index() {
     return (
         <>
-            <div className='bg-[#ccd3e2]'>
-                <div className='text-[#131D32] text-[24px] font-medium xl:text-[1.25vw] pb-[42px] xl:pb-[2.188vw]'>Standard Reports</div>
-                <div className='grid grid-cols-12'>
+            <div className='bg-[#ccd3e2] p-[50px] xl:p-[2.604vw]'>
+                <div className='text-[#131D32] text-[24px] font-medium xl:text-[1.25vw] pb-[42px] xl:pb-[2.188vw]  '>Standard Reports</div>
+                <div className='grid grid-cols-12 gap-[45px] xl:gap-[2.344vw]'>
                     <div className='col-span-3'>
-                        <div className='shadow-1 shadow-[#1018281A] bg-white '>
-                            <div className='flex '>
-                                <div>
-                                <div className='text-[#667085] text-[12px] xl:text-[0.625vw] '> Standard Reports</div>
-                            <div className='text-[#101828] text-[24px] xl:text-[1.25vw]'>Assets</div>
+                        <div className='shadow-xl shadow-[#1018281A] bg-white  rounded-[4px]'>
+                            <div className='flex items-center '>
+                                <div className='px-[25px] xl:px-[1.302vw]'>
+                                    <div className='text-[#667085] text-[12px] xl:text-[0.625vw] uppercase'> Standard Reports</div>
+                                    <div className='text-[#101828]  text-[24px] xl:text-[1.25vw] border border-[#C8CBD0] px-[12px] py-[8px] xl:px-[0.625vw] xl:py-[0.417vw] rounded-[4px] mt-[7px] xl:mt-[0.365vw]'> ASSETS</div>
                                 </div>
-                            <div className=''>
-                            <Image className='mx-auto my-5' height={100} width={150} src={"/images/assets/assetsimg.png"} alt={"imgaes1"}></Image>
+                                <div className=''>
+                                    <Image className='mt-5 mb-2 h-[110px]' height={80} width={160} src={"/assets/images/assetsimg.png"} alt={"imgaes1"}></Image>
+                                </div>
                             </div>
+                            <Image className='rounded-b-[4px] ' height={230} width={390} src={"/assets/images/reportImg.svg"} alt={"imgaes"}></Image>
+
+                        </div>
+                    </div>
+                    <div className='col-span-3'>
+                        <div className='shadow-xl shadow-[#1018281A] bg-white  rounded-[4px]'>
+                            <div className='flex items-center '>
+                                <div className='px-[25px] xl:px-[1.302vw]'>
+                                    <div className='text-[#667085] text-[12px] xl:text-[0.625vw] uppercase'> Standard Reports</div>
+                                    <div className='text-[#101828]  text-[24px] xl:text-[1.25vw] border border-[#C8CBD0] px-[12px] py-[8px] xl:px-[0.625vw] xl:py-[0.417vw] rounded-[4px] mt-[7px] xl:mt-[0.365vw] uppercase'> Software</div>
+                                </div>
+                                <div className=''>
+                                    <Image className='mt-5 mb-2 h-[110px]' height={80} width={160} src={"/assets/images/softwareimg.svg"} alt={"softwareimg"}></Image>
+                                </div>
                             </div>
-                            <Image className='mx-auto my-5' height={300} width={500} src={"/images/assets/reportImg.svg"} alt={"imgaes"}></Image>
+                            <Image className='rounded-b-[4px] ' height={230} width={390} src={"/assets/images/reportImg.svg"} alt={"imgaes"}></Image>
+
+                        </div>
+                    </div>
+                    <div className='col-span-3'>
+                        <div className='shadow-xl shadow-[#1018281A] bg-white  rounded-[4px]'>
+                            <div className='flex items-center '>
+                                <div className='px-[25px] xl:px-[1.302vw]'>
+                                    <div className='text-[#667085] text-[12px] xl:text-[0.625vw] uppercase'> Standard Reports</div>
+                                    <div className='text-[#101828]  text-[24px] xl:text-[1.25vw] border border-[#C8CBD0] px-[12px] py-[8px] xl:px-[0.625vw] xl:py-[0.417vw] rounded-[4px] mt-[7px] xl:mt-[0.365vw] uppercase'> Vendor</div>
+                                </div>
+                                <div className=''>
+                                    <Image className='mt-5 mb-2 h-[110px]' height={80} width={160} src={"/assets/images/vendorimg.svg"} alt={"imgaes1"}></Image>
+                                </div>
+                            </div>
+                            <Image className='rounded-b-[4px] ' height={230} width={390} src={"/assets/images/reportImg.svg"} alt={"imgaes"}></Image>
+
+                        </div>
+                    </div>
+                    <div className='col-span-3'>
+                        <div className='shadow-xl shadow-[#1018281A] bg-white  rounded-[4px]'>
+                            <div className='flex items-center '>
+                                <div className='px-[25px] xl:px-[1.302vw]'>
+                                    <div className='text-[#667085] text-[12px] xl:text-[0.625vw] uppercase'> Standard Reports</div>
+                                    <div className='text-[#101828]  text-[24px] xl:text-[1.25vw] border border-[#C8CBD0] px-[12px] py-[8px] xl:px-[0.625vw] xl:py-[0.417vw] rounded-[4px] mt-[7px] xl:mt-[0.365vw] uppercase'> Users</div>
+                                </div>
+                                <div className=''>
+                                    <Image className='mt-5 mb-2 h-[110px]' height={160} width={160} src={"/assets/images/Users.svg"} alt={"imgaes1"}></Image>
+                                </div>
+                            </div>
+                            <Image className='rounded-b-[4px] ' height={230} width={390} src={"/assets/images/reportImg.svg"} alt={"imgaes"}></Image>
+
+                        </div>
+                    </div>
+                    <div className='col-span-3'>
+                        <div className='shadow-xl shadow-[#1018281A] bg-white  rounded-[4px]'>
+                            <div className='flex items-center '>
+                                <div className='px-[25px] xl:px-[1.302vw]'>
+                                    <div className='text-[#667085] text-[12px] xl:text-[0.625vw] uppercase'> Standard Reports</div>
+                                    <div className='text-[#101828]  text-[24px] xl:text-[1.25vw] border border-[#C8CBD0] px-[12px] py-[8px] xl:px-[0.625vw] xl:py-[0.417vw] rounded-[4px] mt-[7px] xl:mt-[0.365vw] uppercase'>Locations</div>
+                                </div>
+                                <div className=''>
+                                    <Image className='mt-5 mb-2 h-[110px]' height={50} width={160} src={"/assets/images/Mapimg.svg"} alt={"imgaes1"}></Image>
+                                </div>
+                            </div>
+                            <Image className='rounded-b-[4px] ' height={230} width={390} src={"/assets/images/reportImg.svg"} alt={"imgaes"}></Image>
+
+                        </div>
+                    </div>
+                    <div className='col-span-3'>
+                        <div className='shadow-xl shadow-[#1018281A] bg-white  rounded-[4px]'>
+                            <div className='flex items-center '>
+                                <div className='px-[25px] xl:px-[1.302vw]'>
+                                    <div className='text-[#667085] text-[12px] xl:text-[0.625vw] uppercase'> Standard Reports</div>
+                                    <div className='text-[#101828]  text-[24px] xl:text-[1.25vw] border border-[#C8CBD0] px-[12px] py-[8px] xl:px-[0.625vw] xl:py-[0.417vw] rounded-[4px] mt-[7px] xl:mt-[0.365vw] uppercase'> Tickets</div>
+                                </div>
+                                <div className=''>
+                                    <Image className='mt-5 mb-2 h-[110px]' height={80} width={160} src={"/assets/images/ticketimg.svg"} alt={"imgaes1"}></Image>
+                                </div>
+                            </div>
+                            <Image className='rounded-b-[4px] ' height={230} width={390} src={"/assets/images/reportImg.svg"} alt={"imgaes"}></Image>
+
+                        </div>
+                    </div>
+                    <div className='col-span-3'>
+                        <div className='shadow-xl shadow-[#1018281A] bg-white  rounded-[4px]'>
+                            <div className='flex items-center '>
+                                <div className='px-[25px] xl:px-[1.302vw]'>
+                                    <div className='text-[#667085] text-[12px] xl:text-[0.625vw] uppercase'> Standard Reports</div>
+                                    <div className='text-[#101828]  text-[24px] xl:text-[1.25vw] border border-[#C8CBD0] px-[12px] py-[8px] xl:px-[0.625vw] xl:py-[0.417vw] rounded-[4px] mt-[7px] xl:mt-[0.365vw] uppercase'>Invoices</div>
+                                </div>
+                                <div className=''>
+                                    <Image className='mt-5 mb-2 h-[110px]' height={80} width={160} src={"/assets/images/innvoiceimg.svg"} alt={"imgaes1"}></Image>
+                                </div>
+                            </div>
+                            <Image className='rounded-b-[4px] ' height={230} width={390} src={"/assets/images/reportImg.svg"} alt={"imgaes"}></Image>
+
+                        </div>
+                    </div>
+                    <div className='col-span-3'>
+                        <div className='shadow-xl shadow-[#1018281A] bg-white  rounded-[4px]'>
+                            <div className='flex items-center '>
+                                <div className='px-[25px] xl:px-[1.302vw]'>
+                                    <div className='text-[#667085] text-[12px] xl:text-[0.625vw] uppercase'> Standard Reports</div>
+                                    <div className='text-[#101828]  text-[24px] xl:text-[1.25vw] border border-[#C8CBD0] px-[12px] py-[8px] xl:px-[0.625vw] xl:py-[0.417vw] rounded-[4px] mt-[7px] xl:mt-[0.365vw] uppercase'>Payments</div>
+                                </div>
+                                <div className=''>
+                                    <Image className='mt-5 mb-2 h-[110px]' height={80} width={160} src={"/assets/images/Payments.svg"} alt={"imgaes1"}></Image>
+                                </div>
+                            </div>
+                            <Image className='rounded-b-[4px] ' height={230} width={390} src={"/assets/images/reportImg.svg"} alt={"imgaes"}></Image>
 
                         </div>
                     </div>
