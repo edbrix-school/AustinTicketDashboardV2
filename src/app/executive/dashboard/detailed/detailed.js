0adb46d1ec670ee2ae99b3ff0d2914fe5965e254
Shahrukh khan <123742184+Shahrukh-HIC074@users.noreply.github.com>
2024-02-20 19:53:33 +0530

    shahrukh

--- src/app/executive/dashboard/detailed/detailed.js
+++ src/app/executive/dashboard/detailed/detailed.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import Link from 'next/link'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { InputText } from "primereact/inputtext";
-import DetailedTeamView from "./detailedteam"
+import { Tag } from 'primereact/tag';
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Checkbox } from "primereact/checkbox";
+
 export default function Detailed() {
 const [inputValue, setInputValue] = useState('');
 const handleChange = (event) => {
@@ -13,12 +17,114 @@ setInputValue('');
 };
 
 const [teamVisible , setteamVisible ] = useState(true);
-  const [technicianVisible, settechnicianVisible] = useState(false);
+const [technicianVisible, settechnicianVisible] = useState(false);
 
-  const toggleVisibility = () => {
-    setteamVisible (!teamVisible );
-    settechnicianVisible(!technicianVisible);
-  };
+const toggleVisibility = () => {
+  setteamVisible (!teamVisible );
+  settechnicianVisible(!technicianVisible);
+};
+
+const [teamtableVisible , setteamtableVisible ] = useState(true);
+const [categoryVisible, setcategoryVisible] = useState(false);
+
+const toggleVisibility2 = () => {
+  setteamtableVisible (!teamtableVisible );
+  setcategoryVisible(!categoryVisible);
+};
+
+
+ // Data table
+ const [rowClick, setRowClick] = useState(true);
+ const [selectedProducts, setSelectedProducts] = useState(null);
+ const [filters, setFilters] = useState(null);
+   const products = [
+       {
+        Teamname: "Routers",
+        Totalticket: "AC - 95826",
+        Serialnumber: "2SR - 5318",
+        AvgtimeresolveticketsdDays: "Local District - Northeast",
+        Numberofunopenedtickets:'Apperson ES',
+        Numberofclosedtickets:'Active',
+        Numberofreroutefrom: "8/6/2022, 10:06:00 AM",
+        Numberofrerouteto: "37.0902° N, 95.7129° W",
+        Averageteamrating:"37.0902° N, 95.7129° W",
+       },
+     ];
+   
+     const SearchFilterTemplate = (options) => {
+       return (
+         <>
+           <div className="SearchFilter 3xl:p-[0.729vw] p-3 3xl:rounded-[0.521vw] rounded-lg bg-white boxshadow01 3xl:space-y-[0.625vw] space-y-3">
+             <span className="p-input-icon-left custm-td-filterserach">
+               <i
+                 className="asetsmng-td-search"
+                 style={{ color: "#9CA1AB", fontSize: "0.8rem" }}
+               />
+               <InputText
+                 placeholder="Search"
+                 className="3xl:h-[1.875vw] h-[36px] placeholder:font-normal placeholder:text-[#9CA1AB] placeholder:3xl:text-[0.729vw] placeholder:text-sm"
+                 style={{ }}
+               />
+             </span>
+             <div className="flex flex-col 3xl:gap-[0.625vw] gap-2">
+               {categories.map((category) => {
+                 return (
+                   <div key={category.key} className="flex items-center">
+                     <Checkbox
+                       inputId={category.key}
+                       name="category"
+                       value={category}
+                       onChange={onCategoryChange}
+                       checked={selectedCategories.some(
+                         (item) => item.key === category.key
+                       )}
+                       className="cutsm_checkbox"
+                     />
+                     <label
+                       htmlFor={category.key}
+                       className="ml-2 font-medium text-[#374151] 3xl:text-[0.833vw] text-sm"
+                     >
+                       {category.name}
+                     </label>
+                   </div>
+                 );
+               })}
+             </div>
+           </div>
+         </>
+       );
+     };
+
+     const categories = [
+       { name: "Option 1", key: "A" },
+       { name: "Option 2", key: "M" },
+       { name: "Option 3", key: "P" },
+       { name: "Option 4", key: "R" },
+       { name: "Option 5", key: "R" },
+     ];
+     const [selectedCategories, setSelectedCategories] = useState([categories[1]]);
+   
+     const onCategoryChange = (e) => {
+       let _selectedCategories = [...selectedCategories];
+   
+       if (e.checked) _selectedCategories.push(e.value);
+       else
+         _selectedCategories = _selectedCategories.filter(
+           (category) => category.key !== e.value.key
+         );
+   
+       setSelectedCategories(_selectedCategories);
+     };
+     const actionNameTemplate = (rowData) => {
+      return (
+        <>
+          <div className="flex items-center 3xl:gap-[0.833vw] gap-3 text-[#888888] 3xl:text-[0.938vw] text-sm cursor-pointer" onClick={toggleVisibility2}>
+            <i className="asetsmng-plus"></i><span>Team 1</span>
+          </div>
+        </>
+      );
+    };
+     /*---Table End--*/
 return (
 <>
 <Tabs className={'executiveCustTavbview'}>
@@ -30,11 +136,25 @@ return (
                <Tab onClick={toggleVisibility}>Technician</Tab>
             </TabList>
             {teamVisible  &&
+            <>
+            {teamtableVisible &&
             <div className="flex items-center gap-3 3xl:gap-[0.625vw]">
               <Link href={''} className="3xl:py-[0.417vw] py-2 3xl:pl-[0.833vw] pl-4 text-[#4B586E] flex items-center gap-1 3xl:gap-[0.208vw]"><i className="asetsmng-home"></i><span>Team</span></Link>
               <Link href={''} className="text-[#9CA1AB] text-xs"><i className="asetsmng-arrow-right"></i></Link>
               <Link href={''} className="text-[#1570EF]"><span>Team</span></Link>
             </div>
+            }
+            {categoryVisible &&
+            <div className="flex items-center gap-3 3xl:gap-[0.625vw]">
+            <Link href={''} className="3xl:py-[0.417vw] py-2 3xl:pl-[0.833vw] pl-4 text-[#4B586E] flex items-center gap-1 3xl:gap-[0.208vw]"><i className="asetsmng-home"></i><span>Team</span></Link>
+            <Link href={''} className="text-[#9CA1AB] text-xs"><i className="asetsmng-arrow-right"></i></Link>
+            <Link href={''} className="text-[#9CA1AB]" onClick={toggleVisibility2}><span>Team 1</span></Link>
+            <Link href={''} className="text-[#9CA1AB] text-xs"><i className="asetsmng-arrow-right"></i></Link>
+            <Link href={''} className="text-[#1570EF]"><span>Category</span></Link>
+            </div>
+            }
+            
+            </>
             }
             {technicianVisible &&
             <div className="bg-[#F9F9FC] rounded-2xl 3xl:rounded-[0.833vw] py-1 3xl:py-[0.208vw] px-3 3xl:px-[0.625vw] text-[#84878D] text-xs font-medium leading-[18px] 3xl:text-[0.625vw] 3xl:leading-[0.938vw]">
@@ -71,7 +191,252 @@ return (
          </div>
       </div>
       <TabPanel>
-        <DetailedTeamView />
+      <div>
+      <Tabs className={'executiveTableInnerTab'}>
+      <div className="3xl:py-[0.417vw] py-2 3xl:px-[1.042vw] px-5">
+      <TabList>
+      <Tab>All</Tab>
+      <Tab>Monthly</Tab>
+      <Tab>Weekly</Tab>
+      </TabList>
+      </div>
+      <div>
+      <TabPanel>
+      {teamtableVisible &&
+      <>
+      <DataTable
+              value={products}
+              className="custTable tableCust custCheckBox"
+              scrollable
+              responsiveLayout="scroll"
+              style={{ width: "100%" }}
+              paginator
+              paginatorTemplate="CurrentPageReport RowsPerPageDropdown PrevPageLink PageLinks NextPageLink custmpaginator"
+              currentPageReportTemplate="Rows per page  {first}-{last} of {totalRecords}"
+              rowsPerPageOptions={[5, 10, 25, 50]}
+              rows={10}
+              onSelectionChange={(e) => setSelectedProducts(e.value)}
+              selectionMode={rowClick ? null : "checkbox"}
+              selection={selectedProducts}
+              filters={filters}
+              globalFilterFields={[
+                "Teamname",
+                "Totalticket",
+                "Serialnumber",
+                "representative.resolved",
+                "AvgtimeresolveticketsdDays",
+                "Numberofunopenedtickets",
+                "Numberofclosedtickets",
+                "Numberofreroutefrom",
+                "Numberofrerouteto",
+                "Averageteamrating",
+              ]}
+              dataKey="id">
+              <Column
+                field="Teamname"
+                header="Team Name"
+                body={actionNameTemplate}
+                sortable
+                style={{ minWidth: "10rem" }}
+              ></Column>
+              <Column
+                field="Totalticket"
+                header="Total Ticket"
+                sortable
+                style={{ minWidth: "10rem" }}
+              ></Column>
+              <Column
+                field="Serialnumber"
+                header="Serial Number"
+                sortable
+                style={{ minWidth: "10rem" }}
+              ></Column>
+              <Column
+                field="AvgtimeresolveticketsdDays"
+                header="Avg Time to Resolve Tickets (Days)"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Numberofunopenedtickets"
+                header="Number of Unopened Tickets"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Numberofclosedtickets"
+                header="Number of Closed Tickets"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Numberofreroutefrom"
+                header="Number of Re-Route From"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Numberofrerouteto"
+                header="Number of Re-Route To"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Averageteamrating"
+                header="Average Team Rating"
+                style={{ minWidth: "10rem" }}
+                sortable
+              ></Column>
+            </DataTable>
+            </>
+            }
+            {categoryVisible &&
+            <>
+            <DataTable
+              value={products}
+              className="custTable tableCust custCheckBox"
+              scrollable
+              responsiveLayout="scroll"
+              style={{ width: "100%" }}
+              paginator
+              paginatorTemplate="CurrentPageReport RowsPerPageDropdown PrevPageLink PageLinks NextPageLink custmpaginator"
+              currentPageReportTemplate="Rows per page  {first}-{last} of {totalRecords}"
+              rowsPerPageOptions={[5, 10, 25, 50]}
+              rows={10}
+              onSelectionChange={(e) => setSelectedProducts(e.value)}
+              selectionMode={rowClick ? null : "checkbox"}
+              selection={selectedProducts}
+              filters={filters}
+              globalFilterFields={[
+                "Teamname",
+                "Totalticket",
+                "Serialnumber",
+                "representative.resolved",
+                "AvgtimeresolveticketsdDays",
+                "Numberofunopenedtickets",
+                "Numberofclosedtickets",
+                "Numberofreroutefrom",
+                "Numberofrerouteto",
+                "Averageteamrating",
+              ]}
+              dataKey="id">
+              <Column field="Teamname"
+                header="Category"
+                sortable
+                style={{ minWidth: "10rem" }}
+              ></Column>
+              <Column
+                field="Totalticket"
+                header="Total Ticket"
+                sortable
+                style={{ minWidth: "10rem" }}
+              ></Column>
+              <Column
+                field="Serialnumber"
+                header="Resolved"
+                sortable
+                style={{ minWidth: "10rem" }}
+              ></Column>
+              <Column
+                field="AvgtimeresolveticketsdDays"
+                header="Avg Time to Resolve Tickets (Days)"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Numberofunopenedtickets"
+                header="Number of Unopened Tickets"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Numberofclosedtickets"
+                header="Number of Closed Tickets"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Numberofreroutefrom"
+                header="Number of Re-Route From"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Numberofrerouteto"
+                header="Number of Re-Route To"
+                showFilterMatchModes={false}
+                showFilterMenuOptions={false}
+                showClearButton={false}
+                showApplyButton={false}
+                filter
+                filterElement={SearchFilterTemplate}
+                style={{ minWidth: "12rem" }}
+              ></Column>
+              <Column
+                field="Averageteamrating"
+                header="Average Team Rating"
+                style={{ minWidth: "10rem" }}
+                sortable
+              ></Column>
+            </DataTable>
+            </>
+            }
+            
+      </TabPanel>
+      <TabPanel>
+      Monthly Table
+      </TabPanel>
+      <TabPanel>
+      Weekly Table
+      </TabPanel>
+      </div>
+      </Tabs>
+      </div>
       </TabPanel>
       <TabPanel>
          Technician Table
--- src/app/executive/dashboard/detailed/detailedteam.js
+++ src/app/executive/dashboard/detailed/detailedteam.js
@@ -6,28 +6,7 @@ import ExecutivedetailedteamTableView from "./executivedetailedteamtable"
 export default function DetailedTeamView() {
 return (
 <>
-<div>
-<Tabs className={'executiveTableInnerTab'}>
-    <div className="3xl:py-[0.417vw] py-2 3xl:px-[1.042vw] px-5">
-    <TabList>
-      <Tab>All</Tab>
-      <Tab>Monthly</Tab>
-      <Tab>Weekly</Tab>
-    </TabList>
-    </div>
-    <div>
-    <TabPanel>
-      <ExecutivedetailedteamTableView />
-    </TabPanel>
-    <TabPanel>
-    <ExecutivedetailedteamTableView />
-    </TabPanel>
-    <TabPanel>
-    <ExecutivedetailedteamTableView />
-    </TabPanel>
-    </div>
-  </Tabs>
-</div>
+
 </>
 )
 }
