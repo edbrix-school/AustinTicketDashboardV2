60191dc18aa6ec855d687d529ebe9d4df1ef7def
Shahrukh khan <123742184+Shahrukh-HIC074@users.noreply.github.com>
2024-02-21 17:34:30 +0530

    shahrukh

---
+++ src/app/executive/dashboard/detailed/teamtreetable.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Column } from "primereact/column";
+import { TreeTable } from 'primereact/treetable';
+import { DetailsTree } from './treetabledata';
+
+export default function TeamtreetableComponent() {
+const [nodes, setNodes] = useState([]);
+
+useEffect(() => {
+DetailsTree.getTreeTableNodes().then((data) => setNodes(data));
+}, []);
+return (
+<>
+    <TreeTable value={nodes} tableStyle={{ minWidth: '100%' }} 
+    className="custTable tableCust custCheckBox"
+    paginator
+    paginatorTemplate="CurrentPageReport RowsPerPageDropdown PrevPageLink PageLinks NextPageLink custmpaginator"
+    currentPageReportTemplate="Rows per page  {first}-{last} of {totalRecords}"
+    rowsPerPageOptions={[5, 10, 25, 50]}
+    rows={10}>
+    <Column field="teamname" header="Team Name" expander sortable style={{ width: "10rem" }}></Column>
+    <Column field="totalticket" header="Total Ticket" sortable style={{ width: "10rem" }}></Column>
+    <Column field="resolved" header="Resolved" sortable style={{ width: "10rem" }}></Column>
+    <Column field="avgtimeresolveticketsdays" header="Avg Time to Resolve Tickets (Days)" style={{ width: "15rem" }} sortable></Column>
+    <Column field="numberunopenedtickets" header="Number of Unopened Tickets" style={{ width: "12rem" }} sortable></Column>
+    <Column field="numberclosedtickets" header="Number of Closed Tickets" style={{ width: "12rem" }} sortable></Column>
+    <Column field="numberreroutefrom" header="Number of Re-Route From" style={{ width: "12rem" }} sortable></Column>
+    <Column field="numberrerouteto" header="Number of Re-Route To" style={{ width: "12rem" }} sortable></Column>
+    <Column field="averageteamrating" header="Average Team Rating" style={{ width: "12rem" }} sortable></Column>
+    </TreeTable>
+</>
+)
+}
---
+++ src/app/executive/dashboard/detailed/treetabledata.js
@@ -0,0 +1,291 @@
+export const DetailsTree = {
+    getTreeTableNodesData() {
+        return [
+            {
+                key: '0',
+                data: {
+                    teamname: 'Team 1',
+                    totalticket: '100',
+                    resolved: '40 (40%)',
+                    avgtimeresolveticketsdays: '4.5',
+                    numberunopenedtickets: '10 (10%)',
+                    numberclosedtickets: '5 (5%)',
+                    numberreroutefrom:'20',
+                    numberrerouteto:'10',
+                    averageteamrating:'3'
+                },
+                children: [
+                    {
+                        key: '0-0',
+                        data: {
+                            teamname: 'Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                        },
+                        children: [
+                            {
+                                key: '0-0-0',
+                                data: {
+                                    teamname: 'Sub-Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                                }
+                            },
+                            {
+                                key: '0-0-0',
+                                data: {
+                                    teamname: 'Sub-Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                                }
+                            },
+                            {
+                                key: '0-0-0',
+                                data: {
+                                    teamname: 'Sub-Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                                }
+                            },
+                            {
+                                key: '0-0-0',
+                                data: {
+                                    teamname: 'Sub-Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                                }
+                            },
+                            
+                        ]
+                    },
+                ]
+            },
+            {
+                key: '2',
+                data: {
+                    teamname: 'Team 1',
+                    totalticket: '100',
+                    resolved: '40 (40%)',
+                    avgtimeresolveticketsdays: '4.5',
+                    numberunopenedtickets: '10 (10%)',
+                    numberclosedtickets: '5 (5%)',
+                    numberreroutefrom:'20',
+                    numberrerouteto:'10',
+                    averageteamrating:'3'
+                },
+                children: [
+                    {
+                        key: '0-0',
+                        data: {
+                            teamname: 'Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                        },
+                        children: [
+                            {
+                                key: '0-0-0',
+                                data: {
+                                    teamname: 'Sub-Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                                }
+                            },
+                            
+                        ]
+                    },
+                ]
+            },
+            {
+                key: '3',
+                data: {
+                    teamname: 'Team 1',
+                    totalticket: '100',
+                    resolved: '40 (40%)',
+                    avgtimeresolveticketsdays: '4.5',
+                    numberunopenedtickets: '10 (10%)',
+                    numberclosedtickets: '5 (5%)',
+                    numberreroutefrom:'20',
+                    numberrerouteto:'10',
+                    averageteamrating:'3'
+                },
+                children: [
+                    {
+                        key: '0-0',
+                        data: {
+                            teamname: 'Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                        },
+                        children: [
+                            {
+                                key: '0-0-0',
+                                data: {
+                                    teamname: 'Sub-Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                                }
+                            },
+                            
+                        ]
+                    },
+                ]
+            },
+            {
+                key: '4',
+                data: {
+                    teamname: 'Team 1',
+                    totalticket: '100',
+                    resolved: '40 (40%)',
+                    avgtimeresolveticketsdays: '4.5',
+                    numberunopenedtickets: '10 (10%)',
+                    numberclosedtickets: '5 (5%)',
+                    numberreroutefrom:'20',
+                    numberrerouteto:'10',
+                    averageteamrating:'3'
+                },
+                children: [
+                    {
+                        key: '0-0',
+                        data: {
+                            teamname: 'Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                        },
+                        children: [
+                            {
+                                key: '0-0-0',
+                                data: {
+                                    teamname: 'Sub-Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                                }
+                            },
+                            
+                        ]
+                    },
+                ]
+            },
+            {
+                key: '5',
+                data: {
+                    teamname: 'Team 1',
+                    totalticket: '100',
+                    resolved: '40 (40%)',
+                    avgtimeresolveticketsdays: '4.5',
+                    numberunopenedtickets: '10 (10%)',
+                    numberclosedtickets: '5 (5%)',
+                    numberreroutefrom:'20',
+                    numberrerouteto:'10',
+                    averageteamrating:'3'
+                },
+                children: [
+                    {
+                        key: '0-0',
+                        data: {
+                            teamname: 'Category 1',
+                            totalticket: '100',
+                            resolved: '40 (40%)',
+                            avgtimeresolveticketsdays: '4.5',
+                            numberunopenedtickets: '10 (10%)',
+                            numberclosedtickets: '5 (5%)',
+                            numberreroutefrom:'20',
+                            numberrerouteto:'10',
+                            averageteamrating:'3'
+                        },
+                        children: [
+                            {
+                                key: '0-0-0',
+                                data: {
+                                teamname: 'Sub-Category 1',
+                                totalticket: '100',
+                                resolved: '40 (40%)',
+                                avgtimeresolveticketsdays: '4.5',
+                                numberunopenedtickets: '10 (10%)',
+                                numberclosedtickets: '5 (5%)',
+                                numberreroutefrom:'20',
+                                numberrerouteto:'10',
+                                averageteamrating:'3'
+                                }
+                            },
+                            
+                        ]
+                    },
+                ]
+            },
+        ];
+    },
+
+    getTreeTableNodes() {
+        return Promise.resolve(this.getTreeTableNodesData());
+    },
+
+    getTreeNodes() {
+        return Promise.resolve(this.getTreeNodesData());
+    }
+};
