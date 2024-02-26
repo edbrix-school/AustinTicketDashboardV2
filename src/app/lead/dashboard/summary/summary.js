2950900b7b7443e18ea53cb07ffa2d7d23bd8165
Shahrukh khan <123742184+Shahrukh-HIC074@users.noreply.github.com>
2024-02-26 17:25:31 +0530

    shahrukh

--- src/app/lead/dashboard/summary/summary.js
+++ src/app/lead/dashboard/summary/summary.js
@@ -383,11 +383,74 @@ export default function summary() {
       rating: '4.5'
     },
   ]
+
+  const cardData = [
+    {
+      id: 1,
+      title: 'Total Tickets Assigned',
+      count: '1000',
+      bylastperioddata: '2%'
+    },
+    {
+      id: 2,
+      title: 'Tickets in Awaiting User Input',
+      count: '80',
+      countsmall: '(8%)',
+      bylastperioddata: '2%'
+    },
+    {
+      id: 3,
+      title: 'Tickets In-Progress',
+      count: '560',
+      countsmall: '(56%)',
+      bylastperioddata: '2%'
+    },
+    {
+      id: 4,
+      title: 'Tickets Resolved',
+      count: '120',
+      countsmall: '(12%)',
+      bylastperioddata: '2%'
+    },
+    {
+      id: 5,
+      title: 'Average Feedback Ratings',
+      count: '3.5',
+      bylastperioddata: '2%'
+    },
+  ]
  
 
   return (
     <div>
-      {" "}
+      <div className='mt-[16px] 3xl:mt-[0.833vw]'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-[20px] 3xl:gap-[1.042vw]'>
+          {cardData.map((elm) => {
+            return (
+              <div key={elm.id} className='border border-[#E4E7EC] rounded-lg tileShow bg-[#FFFFFF]'>
+                <div className='px-[16px] 3xl:px-[0.833vw] py-[12px] 3xl:py-[0.625vw]'>
+                  <div className='text-[#20232E] text-[16px] 3xl:text-[0.833vw] font-normal'>
+                    {elm.title}
+                  </div>
+                </div>
+                <div className='px-[16px] 3xl:px-[0.833vw] py-[12px] 3xl:py-[0.625vw]'>
+                  <div className='flex justify-between gap-1'>
+                    <div className='text-[#3A3E49] text-[36px] 3xl:text-[1.875vw] font-medium leading-10 3xl:leading-[2.083vw] flex items-end space-x-1'>
+                      <div>{elm.count}</div>
+                      <div className='3xl:text-[1.042vw] text-lg font-normal 3xl:leading-[1.25vw] leading-6'>{elm.countsmall}</div>
+                    </div>
+                    <div className='text-end'>
+                      <div className='text-[#20232E] text-[16px] 3xl:text-[0.833vw] font-medium'>{elm.bylastperioddata}  <i className='asetsmng-arrow-up-circle-fill text-[16px] 3xl:text-[0.833vw] text-[#057A55]'></i></div>
+                      <div className='text-[#84878D] text-[12px] 3xl:text-[0.625vw] font-normal'>by last period</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+
+        </div>
+      </div>
       <div className="grid grid-cols-2 xl:gap-[1.250vw] gap-5 xl:mt-[1.458vw] mt-[24px]">
         {/**col**/}
         <div className="bg-white border border-[#EAEDF3] shadow-md rounded-2xl xl:rounded-[0.833vw] ">
